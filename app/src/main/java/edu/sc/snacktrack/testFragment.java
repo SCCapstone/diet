43516311cec02b48019f1e4f3e6b64f1fa19d3f2
Steven Spitzform <spitzfor@email.sc.edu>
2015-11-02 21:33:56 -0500

    Commit to push missing files (testFragment.java, drawer_list_item.xml, fragment_layout.xml)

---
+++ app/src/main/java/edu/sc/snacktrack/testFragment.java
@@ -0,0 +1,30 @@
+package edu.sc.snacktrack;
+
+/**
+ * Created by Steven on 11/1/2015.
+ */
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class testFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        int position = getArguments().getInt("position");
+        String[] tests = getResources().getStringArray(R.array.tests);
+        View v = inflater.inflate(R.layout.fragment_layout, container, false);
+        TextView tv = (TextView) v.findViewById(R.id.tv_content);
+
+        tv.setText(tests[position]);
+        getActivity().getActionBar().setTitle(tests[position]);
+
+        return v;
+    }
+}
