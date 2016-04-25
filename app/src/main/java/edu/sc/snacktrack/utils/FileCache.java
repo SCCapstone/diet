61dd881908c7ab1d0c231139662faad4c4d988e4
pettenge <pettenge@email.sc.edu>
2016-04-25 12:19:56 -0400

    Documentation for FileCache.

--- app/src/main/java/edu/sc/snacktrack/utils/FileCache.java
+++ app/src/main/java/edu/sc/snacktrack/utils/FileCache.java
@@ -1,17 +1,25 @@
 package edu.sc.snacktrack.utils;
 
-/**
- * Created by dowdw on 11/9/2015.
- */
 import java.io.File;
 import java.io.IOException;
 
 import android.content.Context;
 
+/**
+ * This class represents a file cache, primarily for pictures downloaded from the Internet.
+ */
 public class FileCache {
 
+    /**
+     * The directory of the file cache.
+     */
     private File cacheDir;
 
+    /**
+     * Creates a new FileCache for a specific context.
+     *
+     * @param context The context.
+     */
     public FileCache(Context context) {
         // Find the dir to save cached images
         if (android.os.Environment.getExternalStorageState().equals(
@@ -25,6 +33,12 @@ public class FileCache {
             cacheDir.mkdirs();
     }
 
+    /**
+     * Gets the file for a specific url. The file may not exist or be incomplete.
+     *
+     * @param url The url the file was downloaded from.
+     * @return The file object
+     */
     public File getFile(String url) {
         String filename = String.valueOf(url.hashCode());
         // String filename = URLEncoder.encode(url);
@@ -40,7 +54,7 @@ public class FileCache {
      * @param suffix The suffix of the file name
      * @param prefix Typically the file extension (.png, .jpg, etc.)
      * @return The temporary file
-     * @throws IOException
+     * @throws IOException If an error occurs writing to the file.
      */
     public File createTempFile(String suffix, String prefix) throws IOException{
         return File.createTempFile(suffix, prefix, cacheDir);
@@ -57,6 +71,9 @@ public class FileCache {
         return this.createTempFile("temp", "");
     }
 
+    /**
+     * Attempts to delete all files in this file cache.
+     */
     public void clear() {
         File[] files = cacheDir.listFiles();
         if (files == null)
